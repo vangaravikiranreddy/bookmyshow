eef647922ef7f1efedf11f2d1360d9a2fbf13237
ravikiranreddyvanga <vjravikiranreddy@gmail.com>
2023-08-11 18:44:05 +0530

    Added html,css and js file for login.Added WebConfig class which is used to avoid Cross-Origin Resource Sharing (CORS).

---
+++ bookmyshow/src/main/resources/static/js/login.js
@@ -0,0 +1,36 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    const requestData = {
+        email: email,
+        password: password
+    };
+
+    // Call your backend API here
+    fetch("http://localhost:8080/login", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response from the backend API
+
+            console.log('API Response:', data.validUser);
+            const userAuthenticated = data.validUser;
+            if (userAuthenticated) {
+                window.location.href = '../html/movie.html'; // Redirect to the movies page
+            } else {
+                console.log('Invalid Password')
+            }
+            // You can redirect the user to a different page or perform other actions based on the response
+        })
+        .catch(error => {
+            console.error('API Error:', error);
+        });
+});
